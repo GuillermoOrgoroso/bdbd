e55a6f229fd2c0767569ec8b007b14a08ef0342e
guillermo <guille.bozza@gmail.com>
2020-06-08 15:26:48 -0300

    conexion y obtener los datos de mi bd

--- conectandobdPosta/BDtest/BDtest/Form1.Designer.vb
+++ conectandobdPosta/BDtest/BDtest/Form1.Designer.vb
@@ -22,10 +22,203 @@ Partial Class Form1
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.btnInsertar = New System.Windows.Forms.Button()
+        Me.btnModificar = New System.Windows.Forms.Button()
+        Me.btnEliminar = New System.Windows.Forms.Button()
+        Me.btnBuscar = New System.Windows.Forms.Button()
+        Me.btnSiguiente = New System.Windows.Forms.Button()
+        Me.btnListar = New System.Windows.Forms.Button()
+        Me.btnCortar = New System.Windows.Forms.Button()
+        Me.txtID = New System.Windows.Forms.TextBox()
+        Me.txtMail = New System.Windows.Forms.TextBox()
+        Me.txtApellido = New System.Windows.Forms.TextBox()
+        Me.txtNombre = New System.Windows.Forms.TextBox()
+        Me.lblID = New System.Windows.Forms.Label()
+        Me.lblNombre = New System.Windows.Forms.Label()
+        Me.lblApellido = New System.Windows.Forms.Label()
+        Me.lblMail = New System.Windows.Forms.Label()
+        Me.GrillaDatos = New System.Windows.Forms.DataGridView()
+        CType(Me.GrillaDatos, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnInsertar
+        '
+        Me.btnInsertar.Location = New System.Drawing.Point(270, 51)
+        Me.btnInsertar.Name = "btnInsertar"
+        Me.btnInsertar.Size = New System.Drawing.Size(88, 22)
+        Me.btnInsertar.TabIndex = 0
+        Me.btnInsertar.Text = "Insertar"
+        Me.btnInsertar.UseVisualStyleBackColor = True
+        '
+        'btnModificar
+        '
+        Me.btnModificar.Location = New System.Drawing.Point(270, 77)
+        Me.btnModificar.Name = "btnModificar"
+        Me.btnModificar.Size = New System.Drawing.Size(88, 22)
+        Me.btnModificar.TabIndex = 1
+        Me.btnModificar.Text = "Modificar"
+        Me.btnModificar.UseVisualStyleBackColor = True
+        '
+        'btnEliminar
+        '
+        Me.btnEliminar.Location = New System.Drawing.Point(270, 103)
+        Me.btnEliminar.Name = "btnEliminar"
+        Me.btnEliminar.Size = New System.Drawing.Size(88, 22)
+        Me.btnEliminar.TabIndex = 2
+        Me.btnEliminar.Text = "Eliminar"
+        Me.btnEliminar.UseVisualStyleBackColor = True
+        '
+        'btnBuscar
+        '
+        Me.btnBuscar.Location = New System.Drawing.Point(270, 129)
+        Me.btnBuscar.Name = "btnBuscar"
+        Me.btnBuscar.Size = New System.Drawing.Size(88, 22)
+        Me.btnBuscar.TabIndex = 3
+        Me.btnBuscar.Text = "Buscar"
+        Me.btnBuscar.UseVisualStyleBackColor = True
+        '
+        'btnSiguiente
+        '
+        Me.btnSiguiente.Location = New System.Drawing.Point(270, 155)
+        Me.btnSiguiente.Name = "btnSiguiente"
+        Me.btnSiguiente.Size = New System.Drawing.Size(88, 22)
+        Me.btnSiguiente.TabIndex = 4
+        Me.btnSiguiente.Text = "Siguiente"
+        Me.btnSiguiente.UseVisualStyleBackColor = True
+        '
+        'btnListar
+        '
+        Me.btnListar.Location = New System.Drawing.Point(270, 181)
+        Me.btnListar.Name = "btnListar"
+        Me.btnListar.Size = New System.Drawing.Size(88, 22)
+        Me.btnListar.TabIndex = 5
+        Me.btnListar.Text = "Listar"
+        Me.btnListar.UseVisualStyleBackColor = True
+        '
+        'btnCortar
+        '
+        Me.btnCortar.Location = New System.Drawing.Point(270, 207)
+        Me.btnCortar.Name = "btnCortar"
+        Me.btnCortar.Size = New System.Drawing.Size(88, 22)
+        Me.btnCortar.TabIndex = 6
+        Me.btnCortar.Text = "Cortar"
+        Me.btnCortar.UseVisualStyleBackColor = True
+        '
+        'txtID
+        '
+        Me.txtID.Location = New System.Drawing.Point(123, 53)
+        Me.txtID.Name = "txtID"
+        Me.txtID.Size = New System.Drawing.Size(130, 20)
+        Me.txtID.TabIndex = 7
+        '
+        'txtMail
+        '
+        Me.txtMail.Location = New System.Drawing.Point(123, 131)
+        Me.txtMail.Name = "txtMail"
+        Me.txtMail.Size = New System.Drawing.Size(130, 20)
+        Me.txtMail.TabIndex = 8
+        '
+        'txtApellido
+        '
+        Me.txtApellido.Location = New System.Drawing.Point(123, 105)
+        Me.txtApellido.Name = "txtApellido"
+        Me.txtApellido.Size = New System.Drawing.Size(130, 20)
+        Me.txtApellido.TabIndex = 9
+        '
+        'txtNombre
+        '
+        Me.txtNombre.Location = New System.Drawing.Point(123, 79)
+        Me.txtNombre.Name = "txtNombre"
+        Me.txtNombre.Size = New System.Drawing.Size(130, 20)
+        Me.txtNombre.TabIndex = 10
+        '
+        'lblID
+        '
+        Me.lblID.AutoSize = True
+        Me.lblID.Location = New System.Drawing.Point(29, 56)
+        Me.lblID.Name = "lblID"
+        Me.lblID.Size = New System.Drawing.Size(18, 13)
+        Me.lblID.TabIndex = 11
+        Me.lblID.Text = "ID"
+        '
+        'lblNombre
+        '
+        Me.lblNombre.AutoSize = True
+        Me.lblNombre.Location = New System.Drawing.Point(29, 82)
+        Me.lblNombre.Name = "lblNombre"
+        Me.lblNombre.Size = New System.Drawing.Size(44, 13)
+        Me.lblNombre.TabIndex = 12
+        Me.lblNombre.Text = "Nombre"
+        '
+        'lblApellido
+        '
+        Me.lblApellido.AutoSize = True
+        Me.lblApellido.Location = New System.Drawing.Point(29, 112)
+        Me.lblApellido.Name = "lblApellido"
+        Me.lblApellido.Size = New System.Drawing.Size(44, 13)
+        Me.lblApellido.TabIndex = 13
+        Me.lblApellido.Text = "Apellido"
+        '
+        'lblMail
+        '
+        Me.lblMail.AutoSize = True
+        Me.lblMail.Location = New System.Drawing.Point(29, 138)
+        Me.lblMail.Name = "lblMail"
+        Me.lblMail.Size = New System.Drawing.Size(26, 13)
+        Me.lblMail.TabIndex = 14
+        Me.lblMail.Text = "Mail"
+        '
+        'GrillaDatos
+        '
+        Me.GrillaDatos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.GrillaDatos.Location = New System.Drawing.Point(376, 51)
+        Me.GrillaDatos.Name = "GrillaDatos"
+        Me.GrillaDatos.Size = New System.Drawing.Size(400, 386)
+        Me.GrillaDatos.TabIndex = 15
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        Me.Controls.Add(Me.GrillaDatos)
+        Me.Controls.Add(Me.lblMail)
+        Me.Controls.Add(Me.lblApellido)
+        Me.Controls.Add(Me.lblNombre)
+        Me.Controls.Add(Me.lblID)
+        Me.Controls.Add(Me.txtNombre)
+        Me.Controls.Add(Me.txtApellido)
+        Me.Controls.Add(Me.txtMail)
+        Me.Controls.Add(Me.txtID)
+        Me.Controls.Add(Me.btnCortar)
+        Me.Controls.Add(Me.btnListar)
+        Me.Controls.Add(Me.btnSiguiente)
+        Me.Controls.Add(Me.btnBuscar)
+        Me.Controls.Add(Me.btnEliminar)
+        Me.Controls.Add(Me.btnModificar)
+        Me.Controls.Add(Me.btnInsertar)
+        Me.Name = "Form1"
+        Me.Text = "Gestion"
+        CType(Me.GrillaDatos, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents btnInsertar As Button
+    Friend WithEvents btnModificar As Button
+    Friend WithEvents btnEliminar As Button
+    Friend WithEvents btnBuscar As Button
+    Friend WithEvents btnSiguiente As Button
+    Friend WithEvents btnListar As Button
+    Friend WithEvents btnCortar As Button
+    Friend WithEvents txtID As TextBox
+    Friend WithEvents txtMail As TextBox
+    Friend WithEvents txtApellido As TextBox
+    Friend WithEvents txtNombre As TextBox
+    Friend WithEvents lblID As Label
+    Friend WithEvents lblNombre As Label
+    Friend WithEvents lblApellido As Label
+    Friend WithEvents lblMail As Label
+    Friend WithEvents GrillaDatos As DataGridView
 End Class
--- conectandobdPosta/BDtest/BDtest/Form1.vb
+++ conectandobdPosta/BDtest/BDtest/Form1.vb
@@ -1,3 +1,16 @@
-﻿Public Class Form1
+﻿Imports System.Data.Odbc
+
+
+Public Class Form1
+    Private stringDeConexion As String = "DRIVER=MySQL ODBC 8.0 UNICODE Driver;UID=root;PWD=Tooroot123654;PORT=3307;DATABASE=Persona;SERVER=localhost"
+
+
+    Private Sub obtenerDatos()
+        Dim conexion As New OdbcConnection(stringDeConexion)
+        conexion.Open()
+
+
+    End Sub
+
 
 End Class
